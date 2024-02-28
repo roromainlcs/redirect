463d04a0f172e255eefaa04751e0a70b612eaa28
impe <romain.lucas@epitech.eu>
2024-02-28 12:06:57 +0100

    add verbose

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const port = 3000; // You can use any port that is free on your system
 
 // Redirect all requests to the specific URL
 app.get('*', (req, res) => {
-    //add cookie adn redirect
-    res.cookie('GOSESSION', 'guest-go1.11.5')
-    res.redirect('https://ssrf1.secu-web.blackfoot.dev/secret');
-    //res.status(200).send('Hello World');
+    //add cookie and redirect
     console.log('Request received');
     console.log(req.headers);
+    res.cookie('GOSESSION', 'guest-go1.11.5');
+    res.redirect('https://ssrf1.secu-web.blackfoot.dev/secret');
+    //res.status(200).send('Hello World');
+    console.log('Request received 2:');
+    console.log(req.headers);
+    //add header
+    
 });
 
 // Start the server
